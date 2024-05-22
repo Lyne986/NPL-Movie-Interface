e50e067c624e8c770aeef0c588f09f91f8eacbbe
Thomy Lorenzatti <thomy.lorenzatti@epitech.eu>
2024-05-22 14:10:31 +0900

    Add valid path for vercel

--- front/src/app/api/get-movies/route.ts
+++ front/src/app/api/get-movies/route.ts
@@ -1,8 +1,9 @@
 import fs from 'fs';
+import path from 'path';
 import csvParser from 'csv-parser';
 import { NextResponse } from 'next/server';
 
-const csvName = "./src/data/movie_details.csv";
+const csvName = path.join(process.cwd(), 'src/data/movie_details.csv');
 
 interface Card {
   title: string;
@@ -14,10 +15,10 @@ interface Card {
 
 let filmsList: Array<Card> = [];
 
-const openCsv = async () => {
+const openCsv = async (): Promise<Array<Card>> => {
   return new Promise((resolve, reject) => {
     const films: Array<Card> = [];
-    
+
     fs.createReadStream(csvName)
       .pipe(csvParser())
       .on('data', (row) => {
@@ -32,7 +33,7 @@ const openCsv = async () => {
   });
 };
 
-function searchFilms(query: any) {
+function searchFilms(query: string): Array<Card> {
   const lowerCaseQuery = query.toLowerCase();
   return filmsList.filter(film =>
     film.title.toLowerCase().includes(lowerCaseQuery) ||
@@ -43,23 +44,24 @@ function searchFilms(query: any) {
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
   const title = searchParams.get('title');
-  console.log('GET /api/getMovies', title);
+  console.log('GET /api/get-movies', title);
 
-  if (filmsList.length === 0) {
-    filmsList = await openCsv() as Card[];
+  try {
+    if (filmsList.length === 0) {
+      filmsList = await openCsv();
+    }
+
+    let result: Array<Card> = [];
+
+    if (title) {
+      result = searchFilms(title);
+    } else {
+      result = filmsList;
+    }
+
+    return NextResponse.json({ data: result });
+  } catch (error) {
+    console.error('Error in GET /api/get-movies:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
   }
-
-  let result: any = [];
-
-  if (title) {
-    result = searchFilms(title);
-  }
-
-  if (!title) {
-    result = filmsList;
-  }
-
-  const response = NextResponse.json({ data: result });
-
-  return response;
 }
