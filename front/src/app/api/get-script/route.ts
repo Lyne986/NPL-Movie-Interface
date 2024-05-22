93a9834f8c2c6e8148711c338d29a5b454c33550
Thomy Lorenzatti <thomy.lorenzatti@epitech.eu>
2024-05-22 14:44:56 +0900

    Add icon create route

---
+++ front/src/app/api/get-script/route.ts
@@ -0,0 +1,63 @@
+import fs from 'fs';
+import path from 'path';
+import csvParser from 'csv-parser';
+import { NextResponse } from 'next/server';
+
+const csvName = path.join(process.cwd(), 'src/data/movie_details.csv');
+
+interface Card {
+  title: string;
+  script: string;
+}
+
+let filmsList: Array<Card> = [];
+
+const openCsv = async (): Promise<Array<Card>> => {
+  return new Promise((resolve, reject) => {
+    const films: Array<Card> = [];
+
+    fs.createReadStream(csvName)
+      .pipe(csvParser())
+      .on('data', (row) => {
+        films.push(row);
+      })
+      .on('end', () => {
+        resolve(films);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+};
+
+function searchFilms(query: string): Array<Card> {
+  const lowerCaseQuery = query.toLowerCase();
+  return filmsList.filter(film =>
+    film.title.toLowerCase().includes(lowerCaseQuery)
+  );
+}
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const title = searchParams.get('title');
+  console.log('GET /api/get-script', title);
+
+  try {
+    if (filmsList.length === 0) {
+      filmsList = await openCsv();
+    }
+
+    let result: Array<Card> = [];
+
+    if (title) {
+      result = searchFilms(title);
+    } else {
+      result = filmsList;
+    }
+
+    return NextResponse.json({ data: result });
+  } catch (error) {
+    console.error('Error in GET /api/get-script:', error);
+    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+  }
+}
